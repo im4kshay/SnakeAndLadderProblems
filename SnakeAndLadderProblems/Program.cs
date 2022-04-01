29bc176c0ed4ae241e95dbb733b2d7ec11678603
im4kshay <akshaysayre7@gmail.com>
2022-04-02 04:48:09 +0530

    Add project files.

---
+++ SnakeAndLadderProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Game");
+            Console.ReadLine();
+        }
+    }
+}
